af69addfdbf9beca470953feb30104e39be5cc3e
honbin002-design <honbin002@gmail.com>
2026-08-24 14:45:12 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='hongbin-crm-r54';
+const CACHE='hongbin-crm-r55';
 const CORE=['./','./index.html','./manifest.webmanifest','./quote-letterhead.jpg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
